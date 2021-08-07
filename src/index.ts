55766f347186c65fb5576bb292cb91ad1232542d
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-07 11:57:23 -0300

    refactor: change name for server run

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ const app = express()
 app.use(express.json());
 app.use(routes)
 app.listen(3333, ()=>{
-    console.log("aplicação rodando na porta 3333")
+    console.log("Server is running in port 3333")
 })
 
