9a8bf41814fc49df5b49df5876a36079407a2d3b
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-07 11:56:05 -0300

    feat: add route for consultCep

--- src/routes/Tracking.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from "express";
-
-const trackingRouter = Router();
-
-trackingRouter.get("/", (request: Request, response: Response) => {
-  return response.json({ message: "Running" });
-});
-        
-export default trackingRouter;
